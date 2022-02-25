3475d3ce8a268c8102a4cc540e3ef25a8ef737ad
JL-OLEMAR <joseluis19963@gmail.com>
2022-02-24 23:55:06 -0500

    Add postcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    /* permite anidar reglas de estilo entre sí */
+    'postcss-nesting': true,
+
+    /* agregar prefijos de proveedores a las reglas de CSS */
+    autoprefixer: true
+  }
+}
