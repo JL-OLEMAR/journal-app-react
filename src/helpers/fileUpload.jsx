946bab4c8970b608e3454167ae683fb3411fe362
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-06 19:23:24 -0500

    Change url cloudinary

--- src/helpers/fileUpload.jsx
+++ src/helpers/fileUpload.jsx
@@ -1,6 +1,6 @@
 export const fileUpload = async (file) => {
   // Upload images to server the cloudinary
-  const cloudUrl = `${import.meta.env.VITE_APP_CLOUDINARY_URL}/upload`
+  const cloudUrl = 'https://api.cloudinary.com/v1_1/olemar/upload'
   const formData = new window.FormData()
 
   formData.append('upload_preset', 'react-journal')
