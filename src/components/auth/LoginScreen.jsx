4ba3d73a66111063fe71c503990b490aa1634453
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-08 12:26:00 -0500

    Add validator to Login

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import validator from 'validator'
 
 import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth.jsx'
+import { removeError, setError } from '../../actions/ui.jsx'
 import { useForm } from '../../hooks/useForm.jsx'
 import googleLogo from '../../google-logo.svg'
 
@@ -9,7 +11,7 @@ import './LoginScreen.css'
 
 export const LoginScreen = () => {
   const dispatch = useDispatch()
-  const { loading } = useSelector(state => state.ui)
+  const { loading, msgError } = useSelector(state => state.ui)
 
   const [formValues, handleInputChange] = useForm({
     email: '',
@@ -20,10 +22,31 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault()
-    dispatch(startLoginEmailPassword(email, password))
+
+    if (isFormValid()) {
+      // Login with email and password
+      dispatch(startLoginEmailPassword(email, password))
+    }
   }
 
+  // Login with google
   const handleGoogleLogin = () => dispatch(startGoogleLogin())
+
+  // Valid email and password of the form
+  const isFormValid = () => {
+    if (!validator.isEmail(email)) {
+      dispatch(setError('Email is not valid!'))
+
+      return false
+    } else if (password.length < 5) {
+      dispatch(setError('Password should be at least 6 characters and match each other'))
+
+      return false
+    }
+    dispatch(removeError())
+
+    return true
+  }
 
   return (
     <>
@@ -32,6 +55,8 @@ export const LoginScreen = () => {
         className='animate__animated animate__fadeIn animate__faster'
         onSubmit={handleLogin}
       >
+        {msgError && <div className='alert-error config-center'>{msgError}</div>}
+
         <input
           autoFocus
           autoComplete='off'
