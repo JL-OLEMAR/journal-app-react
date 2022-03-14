986d939541b138cbab113318167d9cc393f69951
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-14 15:51:34 -0500

    Add config vitest

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: ['./vitest.setup.js']
+  }
 })
