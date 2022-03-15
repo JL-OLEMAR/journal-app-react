58f8178c0d86379f331adbb89a4b38e50e31381b
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-15 02:33:02 -0500

    Fix import fetch

--- src/helpers/fileUpload.jsx
+++ src/helpers/fileUpload.jsx
@@ -1,17 +1,15 @@
-import fetch, { FormData } from 'node-fetch'
-
 export const fileUpload = async (file) => {
   // Url of the uploaded file
   const cloudUrl = `${import.meta.env.VITE_APP_CLOUDINARY_URL}/upload`
   // Form data
-  const formData = new FormData()
+  const formData = new window.FormData()
 
   // "react-journal-firebase" is the name of the upload_preset in Cloudinary
   formData.append('upload_preset', 'react-journal-firebase')
   formData.append('file', file)
 
   try {
-    const resp = await fetch(cloudUrl, {
+    const resp = await window.fetch(cloudUrl, {
       method: 'POST',
       body: formData
     })
