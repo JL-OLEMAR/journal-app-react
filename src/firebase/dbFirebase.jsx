80760c361967e018d39503c6e8172dcd89c9815c
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-17 04:52:16 -0500

    Add env for test

--- src/firebase/dbFirebase.jsx
+++ src/firebase/dbFirebase.jsx
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getAnalytics } from 'firebase/analytics'
+// import { getAnalytics } from 'firebase/analytics'
 
 const envKeys = import.meta.env
 
@@ -16,15 +16,29 @@ const firebaseConfig = {
   measurementId: envKeys.VITE_APP_MEASUREMENT_ID
 }
 
-const app = initializeApp(firebaseConfig)
+const firebaseTestConfig = {
+  apiKey: envKeys.VITE_APP_API_KEY_TEST,
+  authDomain: envKeys.VITE_APP_AUTH_DOMAIN_TEST,
+  projectId: envKeys.VITE_APP_PROJECT_ID_TEST,
+  storageBucket: envKeys.VITE_APP_STORAGE_BUCKET_TEST,
+  messagingSenderId: envKeys.VITE_APP_MESSAGING_SENDER_ID_TEST,
+  appId: envKeys.VITE_APP_APP_ID_TEST
+}
+
+let app
+
+(process.env.NODE_ENV === 'test')
+  ? app = initializeApp(firebaseTestConfig) // test
+  : app = initializeApp(firebaseConfig) // dev or prod
+
 const db = getFirestore(app)
 const auth = getAuth(app)
 const googleAuthProvider = new GoogleAuthProvider(app)
-const analytics = getAnalytics(app)
+
+// const analytics = getAnalytics(app)
 
 export {
   db,
   googleAuthProvider,
-  auth,
-  analytics
+  auth
 }
