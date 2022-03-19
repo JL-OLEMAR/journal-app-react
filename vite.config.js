278af5962010be382a72fa2eb3dca98c0d728e56
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-18 22:28:19 -0500

    Fix setupTests.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
-    environment: 'node',
-    setupFiles: 'src/setupTest.jsx'
+    setupFiles: 'src/setupTests.js'
   }
 })
